1ea7f9040922441edc500e3acc5c26329eebdf2f
thakursachin1004 <sachinst000@gmail.com>
2022-05-24 20:05:12 +0530

    removed unused import

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import AnswerSection from "./answerSection";
 import QuizSection from "./quizSection";
 import "../styles/index.css";
